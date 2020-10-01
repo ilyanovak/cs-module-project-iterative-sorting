78d2a6591587d40e95c948b7db523afc9967dbe3
Ilya Novak <ilyanovak@gmail.com>
2020-09-30 23:30:09 -0400

    Assignment 1: Module Project - Iterative Sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,24 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
-
+    for start_item in range(0, len(arr)-1):
+        small_item = start_item
+        for curr_item in range(start_item, len(arr)):
+            if arr[curr_item] < arr[small_item]: small_item = curr_item
+        arr[start_item], arr[small_item] = arr[small_item], arr[start_item]
     return arr
-
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
-
+    # end of list element starts at last element and iterates to second element
+    for end_of_list in reversed(range(1, len(arr))):
+        # current item starts at first element and iterates to end of list element
+        for curr_item in range(end_of_list):
+            # next item is set to element that follows current item
+            next_item = curr_item + 1
+            # if value at current item is greater than value ext next element
+            if arr[curr_item] > arr[next_item]:
+                # switch current item and next item values
+                arr[curr_item], arr[next_item] = arr[next_item], arr[curr_item]
     return arr
 
 '''
@@ -31,11 +30,11 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,18 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    start_item = 0
+    end_item = len(arr) - 1
+    average = end_item // 2
+    while start_item < end_item:
+        if arr[average] == target: return average
+        elif arr[average] > target: end_item = average
+        else: start_item = average
+        average = average // 2
     return -1  # not found
